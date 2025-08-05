4ce10c5de6b2fecf3b5edf6512ffddbc43d84937
AndresOnate <andres.onate@mail.escuelaing.edu.co>
2025-08-04 21:21:11 -0500

    feat:  Data Validation Annotations

--- src/main/java/com/epam/gymapp/dto/TrainerWorkloadRequest.java
+++ src/main/java/com/epam/gymapp/dto/TrainerWorkloadRequest.java
@@ -4,13 +4,32 @@ package com.epam.gymapp.dto;
 import java.io.Serializable;
 import java.time.LocalDate;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
 public class TrainerWorkloadRequest implements Serializable {
+    
+    @NotBlank(message = "Username is required")
     private String username;
+
+    @NotBlank(message = "First name is required")
     private String firstName;
+
+    @NotBlank(message = "Last name is required")
     private String lastName;
+
+    @NotNull(message = "Trainer active status is required")
     private Boolean isActive;
+
+    @NotNull(message = "Training date is required")
     private LocalDate trainingDate;
+
+    @NotNull(message = "Training duration is required")
+    @Min(value = 1, message = "Training duration must be at least 1 minute")
     private Integer trainingDuration;
+
+    @NotNull(message = "Action type is required")
     private ActionType actionType;
 
     public TrainerWorkloadRequest(String username, String firstName, String lastName,
